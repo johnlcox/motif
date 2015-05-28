d195140c0465b6c73712783a4febe6936bbce899
John Leacox <johnlcox@gmail.com>
2015-05-28 18:16:47 -0500

    Remove additional primitive int stuff

--- motif/src/main/java/com/leacox/motif/pattern/PrimitivePattern.java
+++ motif/src/main/java/com/leacox/motif/pattern/PrimitivePattern.java
@@ -9,11 +9,7 @@ public final class PrimitivePattern {
   private PrimitivePattern() {
   }
 
-  public static <R> IntPattern<R> caseInt(int i, IntFunction<R> function) {
-    return IntPattern.of(j -> j == i, function::apply);
-  }
-
-  public static <R> Pattern<Integer, R> caseIntBoxing(int i, IntFunction<R> function) {
+  public static <R> Pattern<Integer, R> caseInt(int i, IntFunction<R> function) {
     return Pattern.of(j -> j == i, function::apply);
   }
 }
